9aaab09aabcb976023e17260d917107a008751be
Pierre Yager <pierre@levosgien.net>
2015-01-20 22:03:30 +0100

    Do not use Rails.cache, removed "helpers" and changed ambiguous symbols to strings in API method calls

--- lib/devise_mailjet/mailjet_list_api_mapper.rb
+++ lib/devise_mailjet/mailjet_list_api_mapper.rb
@@ -4,28 +4,24 @@ module Devise
   module Models
     module Mailjet
       class MailjetListApiMapper
-        LISTS_CACHE_KEY = "devise_mailjet/lists"
-        CONTACTS_CACHE_KEY = "devise_mailjet/contacts"
-
-        # looks the name up in the cache.  if it doesn't find it, looks it up using the api and saves it to the cache
+        # find the list using the mailjet API and save it to a memory cache
         def list_name_to_id(list_name)
-          load_cached_lists
+          @lists ||= {}
           unless @lists.has_key?(list_name)
-            list = mailjet_list.first(name: list_name)
-            list = mailjet_list.create(name: list_name) if list.nil?
-            @lists[list_name] = list.id
-            save_cached_lists
+            l = ::Mailjet::Contactslist.all(name: list_name, limit: 1).first
+            l = ::Mailjet::Contactslist.create(name: list_name) unless l && l.name == list_name
+            @lists[list_name] = l.id
           end
           @lists[list_name]
         end
 
+        # find the contact using the mailjet API and save it to a memory cache
         def contact_email_to_id(email)
-          load_cached_contacts
+          @contacts ||= {}
           unless @contacts.has_key?(email)
-            contact = mailjet_contact.find(email) # email is a valid key for finding contact resources
-            contact = mailjet_contact.create(email: email) if contact.nil?
-            @contacts[email] = contact.id
-            save_cached_contacts
+            c = ::Mailjet::Contact.find(email) # email is a valid key for finding contact resources
+            c = ::Mailjet::Contact.create(email: email) unless c && c.email == email
+            @contacts[email] = c.id
           end
           @contacts[email]
         end
@@ -37,7 +33,7 @@ module Devise
         def subscribe_to_lists(list_names, email)
           walk_recipients(list_names, email) do |lr, list_id, contact_id|
             if lr.nil?
-              mailjet_rcpt.create(list_ID: list_id, contact_ID: contact_id, is_active: true)
+              ::Mailjet::Listrecipient.create('ListID' => list_id, 'ContactID' => contact_id, is_active: true)
             elsif lr.is_unsubscribed
               lr.is_unsubscribed = false
               lr.is_active = true
@@ -71,46 +67,9 @@ module Devise
           list_names = [list_names] unless list_names.is_a?(Array)
           list_names.each do |list_name|
             list_id = list_name_to_id(list_name)
-            lr = mailjet_rcpt.first('ContactsList' => list_id, 'Contact' =>  contact_id)
+            lr = ::Mailjet::Listrecipient.all('ContactsList' => list_id, 'Contact' =>  contact_id, limit: 1).first
             yield lr, list_id, contact_id if block_given?
           end
-        end
-
-        # load the list from the cache
-        def load_cached_lists
-          @lists ||= Rails.cache.fetch(LISTS_CACHE_KEY) do
-            {}
-          end.dup
-        end
-
-        # save the modified list back to the cache
-        def save_cached_lists
-          Rails.cache.write(LISTS_CACHE_KEY, @lists)
-        end
-
-        # load contacts from the cache
-        def load_cached_contacts
-          @contacts ||= Rails.cache.fetch(CONTACTS_CACHE_KEY) do
-            {}
-          end.dup
-        end
-
-        # save the modified contacts back to the cache
-        def save_cached_contacts
-          Rails.cache.write(CONTACTS_CACHE_KEY, @contacts)
-        end
-
-        # the mailjet api helpers
-        def mailjet_contact
-          ::Mailjet::Contact
-        end
-
-        def mailjet_list
-          ::Mailjet::Contactslist
-        end
-
-        def mailjet_rcpt
-          ::Mailjet::Listrecipient
         end
       end
     end
